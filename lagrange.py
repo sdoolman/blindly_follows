a25128903d881911b20c2599e114f697c34bd802
stavd <stavd@home>
2019-11-17 03:11:41 +0200

    * Added lagrange interpolation example module * Added Shamir's secret sharing example module (from Wiki) * Added a FSM graph generation * Started implementing the variant polynomial calculation to later be used with CRR class

---
+++ lagrange.py
@@ -0,0 +1,68 @@
+import sys
+
+import matplotlib.pyplot as plt
+
+
+def main():
+    if len(sys.argv) == 1 or "-h" in sys.argv or "--help" in sys.argv:
+        print('python lagrange.py <x1.y1> .. <x_k.y_k>')
+
+        print('Example:')
+        print('python lagrange.py 0.1 2.4 4.5 3.2')
+        exit()
+    points = []
+    for i in range(len(sys.argv)):
+        if i != 0:
+            points.append((int(sys.argv[i].split(".")[0]), int(sys.argv[i].split(".")[1])))
+
+    # points =[(0,0),(25,30),(50,10), (57,0)]
+    P = lagrange(points)
+    nr = 2
+    print('(' + str(points[nr][0]) + ', ' + str(points[nr][1]) + ') P(' + str(points[nr][0]) + ')= ' + str(
+        P(points[nr][0])))
+    plot(P, points)
+
+
+def plot(f, points):
+    x = list(range(0, 100))
+    y = map(f, x)
+    print(y)
+    plt.plot(x, list(y), linewidth=2.0)
+    x_list = []
+    y_list = []
+    for x_p, y_p in points:
+        x_list.append(x_p)
+        y_list.append(y_p)
+    print(x_list)
+    print(y_list)
+    plt.plot(x_list, y_list, 'ro')
+
+    plt.show()
+
+
+def lagrange(points):
+    def P(x):
+        total = 0
+        n = len(points)
+        for i in range(n):
+            xi, yi = points[i]
+
+            def g(i, n):
+
+                tot_mul = 1
+                for j in range(n):
+                    if i == j:
+                        continue
+                    xj, yj = points[j]
+                    tot_mul *= (x - xj) / float(xi - xj)
+
+                return tot_mul
+
+            total += yi * g(i, n)
+        return total
+
+    return P
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+import random
+from timeit import default_timer as timer
+
 import numpy as np
+from transitions.extensions import GraphMachine as Machine
+from transitions.extensions.states import add_state_features, Tags
 
 from crr import CRR
 
+
 ###################################################################################################
+
+def print_done(start_val):
+    global elapsed
+    elapsed = timer() - start_val
+    print('< done, time elapsed: [{:.2f}] seconds'.format(elapsed))
+
+
+def print_start(msg):
+    print('> starting {:s}...'.format(msg))
+
 
 if __name__ == '__main__':
     import argparse
@@ -17,10 +33,60 @@ if __name__ == '__main__':
     print(crr.add(x_vec, y_vec))
     print(crr.mul(x_vec, y_vec))
 
-    '''
-    Input/State	0	1
-    0       	0	0
-    1       	1	1
-    f(x,y) = x * y + x + y (mod 2)
-    
-    '''
+    print_start('state machine data generation')
+    start = timer()
+    states = range(1, 10)
+    transitions = dict()
+    for src in states:
+        transitions[src] = (
+            {0: random.choice(states), 1: random.choice(states)},
+            random.randint(0, 1)
+        )
+
+    x_s, y_s = zip(*([(int('{:d}{:d}'.format(src, 0)),
+                       int('{:d}{:d}'.format(t[0][0], t[1]))) for src, t in transitions.items()] + \
+                     [(int('{:d}{:d}'.format(src, 1)),
+                       int('{:d}{:d}'.format(t[0][1], t[1]))) for src, t in transitions.items()]))
+
+    print(['{:d}.{:d}'.format(x, y) for x, y in zip(x_s, y_s)])
+
+    print_done(start)
+
+    print_start('state machine drawing')
+    start = timer()
+
+
+    @add_state_features(Tags)
+    class CustomStateMachine(Machine):
+        pass
+
+
+    class Matter(object):
+        pass
+
+
+    lump = Matter()
+    m = CustomStateMachine(
+        model=lump,
+        states=[
+            {'name': str(src), 'tags': ['out: {:d}'.format(trans[1])]} for src, trans in transitions.items()
+        ],
+        transitions=[{'trigger': 'in: {:d}'.format(0),
+                      'source': str(src),
+                      'dest': str(trans[0][0]),
+                      'after': str(trans[1])}
+                     for src, trans in transitions.items()] +
+                    [{'trigger': 'in: {:d}'.format(1),
+                      'source': str(src),
+                      'dest': str(trans[0][1]),
+                      'after': str(trans[1])}
+                     for src, trans in transitions.items()],
+        initial=str(states[0]),
+        show_state_attributes=True
+    )
+    m.get_graph().draw('diagram.png', prog='dot')
+
+    elapsed = timer() - start
+    print_done(start)
+
+    start = timer()
