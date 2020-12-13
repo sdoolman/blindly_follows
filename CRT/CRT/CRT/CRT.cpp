3fe28735fa6f419f74fc5edd1fb3792252e9ff0e
stavd <stavd@home>
2020-12-14 00:40:08 +0200

    * Added a struct which serves as bit field - all operations are done modulo 2^16 * Added a function which calculates x^2 * Added a simple example which calculates x^2 for 4 different moduli

--- CRT/CRT/CRT/CRT.cpp
+++ CRT/CRT/CRT/CRT.cpp
@@ -1,6 +1,46 @@
+#include <cstdint>
 #include <iostream>
+#include <vector>
+#include <numeric>
 
-int main()
+typedef struct GF65536_t {
+	uint16_t v : 16;
+
+	GF65536_t& operator*=(const GF65536_t& rhs)
+	{
+		v *= rhs.v;
+		return *this;
+	}
+
+	GF65536_t& operator+=(const GF65536_t& rhs)
+	{
+		v += rhs.v;
+		return *this;
+	}
+} GF65536;
+
+GF65536 foo(const GF65536& x)
 {
-    std::cout << "Hello World!\n";
+	// calculate x^2
+	GF65536 res = x;
+	res *= x;
+	return res;
+}
+
+
+int main(void)
+{
+	std::vector<GF65536> items = {
+		{ 20123 },
+		{ 34563 },
+		{ 23489 },
+		{ 53480 }
+	};
+
+	for (auto const& i : items) {
+		auto res = foo(i);
+		std::cout << res.v << " ";
+	}
+
+	return 0;
 }
