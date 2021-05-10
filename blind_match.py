192b0c1c2e8c106324304bd51f25b2ee1c73283b
stavdoo <stavdoo@LPSTAV>
2021-05-10 23:11:51 +0300

    * Added bitstring package * Using yapf as default formatter * Added main method in blind_match module

--- blind_match.py
+++ blind_match.py
@@ -3,16 +3,26 @@ from bitstring import BitArray
 T = [
     (BitArray(bin='101001'), BitArray(bin='101010')),
     (BitArray(bin='010110'), BitArray(bin='010101')),
-]
+]  # must be hashable
 
 
 def match(x: str) -> str:
     x = BitArray(bin=x)
     res = BitArray(length=len(x))
-    for k, v in T:
+    for current_state, next_state in T:
         tmp = BitArray(bin='1')
         for i in range(len(x))[::2]:
-            tmp = tmp & BitArray(bool=((x[i] & k[i]) | (x[i + 1] & k[i + 1])))
-        res |= (tmp * len(v)) & v
+            tmp = tmp & BitArray(bool=x[i] & current_state[i]
+                                 | x[i + 1] & current_state[i + 1])
+        res |= (tmp * len(next_state)) & next_state
 
     return res.bin
+
+
+if __name__ == '__main__':
+    print('> transition table is:\n' + '\n'.join(f'{x} --> {y}' for x, y in T))
+    x = input('> enter state: ')
+    try:
+        print(f'next state is: {match(x)}')
+    except ValueError as e:
+        print(f'invalid state, error: [{e}]')
