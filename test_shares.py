8c676e5ff6d228c06ffe43ceb9fd7b11a3e31db1
stavdoo <stavdoo@LPSTAV>
2021-05-22 16:50:29 +0300

    * Rearranged test_shares

--- test_shares.py
+++ test_shares.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
 import itertools
-#######################################################################################################################
 from sys import stdout
 
 import numpy as np
@@ -9,28 +7,7 @@ from crr.mathlib import garner_algorithm
 from crr.polymod import Mod
 
 
-def f(field_modulo, unknown_shares_file, *known_shares_files):
-    def extract_shares(shares_file):
-        freq = np.memmap(shares_file, mode='r', dtype=np.int64)
-        return np.where(freq != 0)[0], np.size(freq)
-
-    known_shares_col, known_mods = zip(*[extract_shares(sf) for sf in known_shares_files])
-    unknown_shares, unknown_mod = extract_shares(unknown_shares_file)
-
-    secrets = np.zeros(field_modulo)
-    Mod.set_mod(field_modulo)
-    for shares_prod in itertools.product(*known_shares_col):
-        for i, share in enumerate(unknown_shares):
-            stdout.write(f'i={i}/{unknown_mod}\r')
-            res = garner_algorithm(shares_prod + (share,), known_mods + (unknown_mod,))
-            secrets[Mod(res).value] += 1
-
-    fp = np.memmap(f'{field_modulo}.dat', dtype=np.int64, mode='w+', shape=np.size(secrets))
-    fp[:] = secrets[:]  # copy all values
-    fp.flush()
-
-
-if __name__ == '__main__':
+def main():
     import argparse
 
     parser = argparse.ArgumentParser()
@@ -39,4 +16,31 @@ if __name__ == '__main__':
     parser.add_argument('field_modulo', type=int)
 
     args = parser.parse_args()
-    f(args.field_modulo, args.unknown_shares_file, *args.known_shares_files)
+
+    def extract_shares(shares_file):
+        freq = np.memmap(shares_file, mode='r', dtype=np.int64)
+        return np.where(freq != 0)[0], np.size(freq)
+
+    known_shares_col, known_mods = zip(
+        *[extract_shares(sf) for sf in args.known_shares_files])
+    unknown_shares, unknown_mod = extract_shares(args.unknown_shares_file)
+
+    secrets = np.zeros(args.field_modulo)
+    Mod.set_mod(args.field_modulo)
+    for shares_prod in itertools.product(*known_shares_col):
+        for i, share in enumerate(unknown_shares):
+            stdout.write(f'i={i}/{unknown_mod}\r')
+            res = garner_algorithm(shares_prod + (share, ),
+                                   known_mods + (unknown_mod, ))
+            secrets[Mod(res).value] += 1
+
+    fp = np.memmap(f'{args.field_modulo}.dat',
+                   dtype=np.int64,
+                   mode='w+',
+                   shape=np.size(secrets))
+    fp[:] = secrets[:]  # copy all values
+    fp.flush()
+
+
+if __name__ == '__main__':
+    main()
